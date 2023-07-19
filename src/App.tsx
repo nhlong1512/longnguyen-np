88b24a857397b5b8a999bc99af9d4b6cb634419e
nhlong1512 <20521569@gm.uit.edu.vn>
2023-07-19 14:18:56 +0700

    Apply React-router-dom and Mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Exercise1 from "./components/Exercise1";
 import Exercise2 from "./components/Exercise2";
@@ -7,66 +8,67 @@ import Exercise4 from "./components/Exercise4";
 import Exercise5 from "./components/Exercise5";
 import Exercise6 from "./components/Exercise6";
 import Exercise7 from "./components/Exercise7";
+import Exercise8 from "./components/Exercise8";
 
 function App() {
-  const [activeTab, setActiveTab] = useState<string>("Exercise1");
+  const [activeTab, setActiveTab] = useState<string>("exercise1");
   const [tabList, setTabLsit] = useState<string[]>([
-    "Exercise1",
-    "Exercise2",
-    "Exercise3",
-    "Exercise4",
-    "Exercise5",
-    "Exercise6",
-    "Exercise7",
+    "exercise1",
+    "exercise2",
+    "exercise3",
+    "exercise4",
+    "exercise5",
+    "exercise6",
+    "exercise7",
+    "exercise8",
   ]);
 
   const handleTabClick = (tab: string) => {
     setActiveTab(tab);
   };
-
-  const renderComponent = () => {
-    switch (activeTab) {
-      case "Exercise1":
-        return <Exercise1 />;
-      case "Exercise2":
-        return <Exercise2 />;
-      case "Exercise3":
-        return <Exercise3 />;
-      case "Exercise4":
-        return <Exercise4 />;
-      case "Exercise5":
-        return <Exercise5 />;
-      case "Exercise6":
-        return <Exercise6 />;
-      case "Exercise7":
-        return <Exercise7 />;
-      default:
-        return null;
-    }
-  };
   return (
-    <div className="App" style={{position:'relative'}}>
-      <div style={{position: 'absolute', top: '20px', left:'50%', transform: 'translateX(-50%)' }}>
+    <div className="App" style={{ position: "relative" }}>
+      <div
+        style={{
+          position: "absolute",
+          top: "20px",
+          left: "50%",
+          transform: "translateX(-50%)",
+        }}
+      >
         {tabList.map((tab) => (
-          <button
-            onClick={() => handleTabClick(tab)}
-            style={{
-              padding: "10px",
-              fontSize: "16px",
-              fontWeight:'700',
-              borderRadius: "5px",
-              border: "1px solid #ccc",
-              margin: "10px",
-              cursor: "pointer",
-              backgroundColor: activeTab === tab ? "#f0153b" : "#fff",
-              color:activeTab === tab ? "#fff" : '#000',
-            }}
-          >
-            {tab}
-          </button>
+          <Link to={`/${tab}`}>
+            <button
+              onClick={() => handleTabClick(tab)}
+              style={{
+                padding: "10px",
+                fontSize: "16px",
+                fontWeight: "700",
+                borderRadius: "5px",
+                border: "1px solid #ccc",
+                margin: "10px",
+                cursor: "pointer",
+                backgroundColor: activeTab === tab ? "#f0153b" : "#fff",
+                color: activeTab === tab ? "#fff" : "#000",
+              }}
+            >
+              {tab}
+            </button>
+          </Link>
         ))}
       </div>
-      {renderComponent()}
+      {/* {renderComponent()} */}
+      <Routes>
+        <Route path="/" element={<Exercise1 />} />
+        <Route path="/exercise1" element={<Exercise1 />} />
+        <Route path="/exercise2" element={<Exercise2 />} />
+        <Route path="/exercise3" element={<Exercise3 />} />
+        <Route path="/exercise4" element={<Exercise4 />} />
+        <Route path="/exercise5" element={<Exercise5 />} />
+        <Route path="/exercise6" element={<Exercise6 />} />
+        <Route path="/exercise7" element={<Exercise7 />} />
+        <Route path="/exercise8" element={<Exercise8 />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Exercise8.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Exercise8 = () => {
+  return (
+    <div>Exercise8</div>
+  )
+}
+
+export default Exercise8
